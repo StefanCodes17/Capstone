12bc18e2f01762a9b13a5a768ecccb04016ffed8
Stefan <sk893q@gmail.com>
2022-03-29 11:15:54 -0400

    Working on api url routing

--- django-react/frontend/src/pages/Home.js
+++ django-react/frontend/src/pages/Home.js
@@ -1,9 +1,18 @@
-import React from 'react';
+import React, { useEffect } from 'react';
 import Navbar from '../components/Navbar';
 import Main from '../components/Main'
+import axios from 'axios'
+
 
 const HomePage = () => {
-
+	useEffect(()=>{
+		axios({
+			method: 'get',
+			url: '/',
+		  }).then(res =>{
+			  console.log(res)
+		  })
+	}, [])
 	return (
 		<div className='bg-primary_color h-screen w-screen'>
 			<div className="h-screen max-w-screen-2xl overflow-hidden">
