2e3fb7de18546e36144021d3bcc7f64bceca2100
Max Zlotskiy <max.zlotskiy@gmail.com>
2022-04-05 17:33:06 -0400

    render editor

--- django-react/frontend/src/pages/Dashboard.js
+++ django-react/frontend/src/pages/Dashboard.js
@@ -4,12 +4,14 @@ import Navbar from '../components/Navbar'
 import {getUser} from '../state/slices/userSlice'
 import { Navigate, Link } from 'react-router-dom'
 import Sidebar from '../components/Sidebar'
+import DocEditor from '../components/DocEditor';
 
 const Dashboard = () => {
   return (
     <div>  
       <Navbar/>
       <Sidebar />
+      <DocEditor />
     </div>
   )
 }
