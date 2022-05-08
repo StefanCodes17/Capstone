b08f2b5727d643d45c123d2d3bfa4b7985155c2d
Max Zlotskiy <max.zlotskiy@gmail.com>
2022-05-08 02:29:00 -0400

    default page is login, not sign up

--- django-react/frontend/src/components/Navbar.js
+++ django-react/frontend/src/components/Navbar.js
@@ -25,7 +25,7 @@ const Navbar = () => {
 				<div className="flex md:order-2">
 					{/* <Link to={!user?.isLoggedIn ? `signup` : ``} > */}
 						{!user?.isLoggedIn ? (
-							<Link to= "signup">
+							<Link to= "login">
 								<button className="text-white bg-blue-700 hover:bg-blue-800 focus:ring-4 focus:ring-blue-300 font-medium rounded-lg text-sm px-5 py-2.5 text-center mr-3 md:mr-0 dark:bg-blue-600 dark:hover:bg-blue-700 dark:focus:ring-blue-800" type="button">Sign In</button>
 							</Link>
 						):(
