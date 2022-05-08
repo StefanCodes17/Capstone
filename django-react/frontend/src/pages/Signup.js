a7d383eb358d4d4a369a49578111686a81075198
Max Zlotskiy <max.zlotskiy@gmail.com>
2022-05-08 15:10:15 -0400

    removed loading indicator

--- django-react/frontend/src/pages/Signup.js
+++ django-react/frontend/src/pages/Signup.js
@@ -121,10 +121,6 @@ const Signup = () => {
             </div>
             )}
 
-            <div className="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3 shadow-md" role="alert">
-              <p className="font-bold">Signing you up</p>
-            </div>
-
             {loading && (
             <div className="bg-blue-100 border-t border-b border-blue-500 text-blue-700 px-4 py-3 shadow-md" role="alert">
               <p className="font-bold">Signing you up</p>
