d2569cc5b7a032600b276c195728dbfbd4bd9a8b
Stefan <sk893q@gmail.com>
2022-05-16 10:04:46 -0400

    Almost done with right click functionality, can add and remove and rename documents and folders

--- django-react/frontend/src/components/DocTree.js
+++ django-react/frontend/src/components/DocTree.js
@@ -4,13 +4,13 @@ import { MdExpandMore} from "react-icons/md";
 import { AiOutlineRight} from "react-icons/ai";
 import Doc from './Doc'
 
-const Tree=({documents})=>{
+const Tree=({documents, action, setAction})=>{
 
     return(
     <div className="grid row-auto gap-0 relative">
     {
         documents.map(document=>(
-            <Doc document={document}/>
+            <Doc document={document} action={action} setAction={setAction}/>
         ))
     }
     </div>
