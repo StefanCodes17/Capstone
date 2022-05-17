f5da8d2d87726b942ca16f2032794795beb8fbae
Sadab Hafiz <sadabhafizny@gmail.com>
2022-05-17 11:15:40 -0400

    Added strikethrough and toolbar buttons work

--- django-react/frontend/src/components/DocEditor.js
+++ django-react/frontend/src/components/DocEditor.js
@@ -1,8 +1,8 @@
 import React, {useState, useMemo, useCallback} from 'react'
 import { createEditor, Editor, Transforms,Text } from 'slate'
 import { Slate, Editable, withReact } from 'slate-react'
-import {AiOutlineBold, AiOutlineItalic, AiOutlineUnderline} from 'react-icons/ai'
-import {FaBold, FaItalic, FaUnderline, FaFont} from 'react-icons/fa'
+import {AiOutlineBold, AiOutlineItalic, AiOutlineUnderline, AiOutlineStrikethrough} from 'react-icons/ai'
+import {FaBold, FaItalic, FaUnderline, FaFont, FaStrikethrough} from 'react-icons/fa'
 import {BiFont} from 'react-icons/bi'
 import { HexColorPicker } from "react-colorful";
 import isHotkey from 'is-hotkey';
@@ -10,65 +10,12 @@ import isHotkey from 'is-hotkey';
 import useAutosave from '../utils/useAutosave';
 import SentimentView from '../components/SentimentView';
 
-const DocEditorHeader = ({saving, msg})=>{
-
-  const [bold, setBold] = useState(false)
-  const [italics, setItalics] = useState(false)
-  const [underline, setUnderline] = useState(false)
-  const [color, setColor] = useState("#aabbcc");
-  const [displayColor, setDisplayColor] = useState(false)
-
-
-  return(
-    <div>
-      <div className='pt-10 pb-2'>
-        <p className='text-sm text-gray-500 tracking-wide'>{saving ? 'Saving ...' : msg}</p>
-      </div>
-      <div className='flex'>
-        {/* Type of text */}
-        <select name="type" id="type" className="px-6 py-1 border border-lifepad_black focus:outline-none">
-          <option value="paragraph">Paragraph</option>
-          <option value="title">Title</option>
-        </select>
-        {/* Type of font */}
-        <select name="type" id="type" className="px-6 py-1 border border-lifepad_black focus:outline-none">
-          <option value="Arial">Arial</option>
-          <option value="Comic Sans">Comic Sans</option>
-        </select>
-        {/* Font size */}
-        <select name="type" id="type" className="px-6 py-1 border border-lifepad_black focus:outline-none">
-          {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 56, 58, 60, 62, 64, 66, 68, 70, 72].map((num) =>(
-            <option value={num}>{num}px</option>
-          ))}
-        </select>
-        <div className="px-6 py-1 border border-lifepad_black focus:outline-none flex w-fit">
-            <div className="flex pt-1 space-x-2">
-              <div  className='cursor-pointer' onClick={()=> setBold(!bold)}>{!bold ? <AiOutlineBold/> : <FaBold/>}</div>
-              <div  className='cursor-pointer' onClick={() => setItalics(!italics)}>{!italics ? <AiOutlineItalic/> : <FaItalic/>} </div>
-              <div  className='cursor-pointer' onClick={() => setUnderline(!underline)}>{!underline ? <AiOutlineUnderline/> : <FaUnderline/>} </div>
-            </div>
-        </div>
-        <div className="px-6 py-1 border border-lifepad_black focus:outline-none flex w-fit">
-          {/*Color Picker */}
-          <div className='grid place-items-center relative' onClick={() => setDisplayColor(!displayColor)}>
-            {!displayColor ? <BiFont/> : <FaFont/>}
-            <div className='w-6 h-1' style={{backgroundColor: color}}></div>
-            {displayColor &&
-            <div className='absolute -bottom-52 left-0'>
-                <HexColorPicker color={color} onChange={setColor} />
-            </div>}
-          </div>
-        </div>
-      </div>
-    </div>
-  )
-}
-
 const HOTKEYS = {
   'mod+b': 'bold',
   'mod+i': 'italic',
   'mod+u': 'underline',
   'mod+`': 'code',
+  'mod+d': 'strikethrough'
 };
 
 const DocEditor = () => {
@@ -76,6 +23,11 @@ const DocEditor = () => {
     const [selectedText,setSelectedText] = useState("")
     const [saving, setSaving] = useState(true)
     const [msg, setMsg] = useState("")
+    const [color, setColor] = useState("#aabbcc");
+    const [displayColor, setDisplayColor] = useState(false)
+    
+    //Override deselect to avoid losing focus
+    Transforms.deselect = () => {};
 
     const renderElement = useCallback(props => {
         switch (props.element.type) {
@@ -138,23 +90,102 @@ const DocEditor = () => {
   const renderLeaf = useCallback(props => {
     return <Leaf {...props} />
   }, [])
+
   return (
     <div style={{width: "600px", margin: "5px auto"}}>
-    <DocEditorHeader saving={saving} msg={msg}/>
-    <SentimentView className="z-100 left-[70px] top-[40px]" sentimentSentence={fullText} sentimentType="All"/>
-    {selectedText && <SentimentView className="z-100 left-[70px] top-[40px]" sentimentSentence={selectedText} sentimentType="Selected"/>}
+    {/* <DocEditorHeader saving={saving} msg={msg}/> */}
+    
     
 
     <Slate
       editor={editor}
       value={value}
       onChange={ onChangeContent}
-    >
+    > 
+        {/* Toolbar */}
+        <div>
+          <div className='pt-10 pb-2'>
+            <p className='text-sm text-gray-500 tracking-wide'>{saving ? 'Saving ...' : msg}</p>
+          </div>
+          <div className='flex'>
+            {/* Type of text */}
+            <select name="type" id="type" className="px-6 py-1 border border-lifepad_black focus:outline-none">
+              <option value="paragraph">Paragraph</option>
+              <option value="title">Title</option>
+            </select>
+            {/* Type of font */}
+            <select name="type" id="type" className="px-6 py-1 border border-lifepad_black focus:outline-none">
+              <option value="Arial">Arial</option>
+              <option value="Comic Sans">Comic Sans</option>
+            </select>
+            {/* Font size */}
+            <select name="type" id="type" className="px-6 py-1 border border-lifepad_black focus:outline-none">
+              {[1, 2, 3, 4, 5, 6, 7, 8, 9, 10, 12, 14, 16, 18, 20, 22, 24, 26, 28, 30, 32, 34, 36, 38, 40, 42, 44, 46, 48, 50, 52, 56, 58, 60, 62, 64, 66, 68, 70, 72].map((num) =>(
+                <option value={num}>{num}px</option>
+              ))}
+            </select>
+            <div className="px-6 py-1 border border-lifepad_black focus:outline-none flex w-fit">
+                <div className="flex pt-1 space-x-2">
+                  <div  className='cursor-pointer' onMouseDown={(event) => {
+                      event.preventDefault();
+                      toggleMark(editor,'bold');
+                    }
+                  }>
+                    {!isMarkActive(editor,'bold') ? <AiOutlineBold/> : <FaBold/>}
+                  </div>
+                  <div  className='cursor-pointer' onMouseDown={(event) => {
+                      event.preventDefault();
+                      toggleMark(editor,'italic')
+                    }
+                  }>
+                    {!isMarkActive(editor,'italic') ? <AiOutlineItalic/> : <FaItalic/>} 
+                  </div>
+                  <div  className='cursor-pointer' onMouseDown={(event) => {
+                      event.preventDefault();
+                      toggleMark(editor,'underline')
+                    }
+                  }>
+                    {!isMarkActive(editor,'underline')? <AiOutlineUnderline/> : <FaUnderline/>} 
+                  </div>
+                  <div  className='cursor-pointer' onMouseDown={(event) => {
+                      event.preventDefault();
+                      toggleMark(editor,'strikethrough')
+                    }
+                  }>
+                    {!isMarkActive(editor,'strikethrough')? <AiOutlineStrikethrough/> : <FaStrikethrough/>} 
+                  </div>
+                </div>
+            </div>
+            <div className="px-6 py-1 border border-lifepad_black focus:outline-none flex w-fit">
+              {/*Color Picker */}
+              <div className='grid place-items-center relative' onClick={() => setDisplayColor(!displayColor)}>
+                {!displayColor ? <BiFont/> : <FaFont/>}
+                <div className='w-6 h-1' style={{backgroundColor: color}}></div>
+                {displayColor &&
+                <div className='absolute -bottom-52 left-0'>
+                    <HexColorPicker color={color} onChange={setColor} />
+                </div>}
+              </div>
+            </div>
+          </div>
+        </div>
+        {/* Sentiment Analysis */}
+        <SentimentView className="z-100 left-[70px] top-[40px]" sentimentSentence={fullText} sentimentType="All"/>
+        {selectedText && <SentimentView className="z-100 left-[70px] top-[40px]" sentimentSentence={selectedText} sentimentType="Selected"/>}
+
         <Editable 
           renderElement={renderElement}
           renderLeaf={renderLeaf}
           spellCheck={true}
+          autoFocus
           onKeyDown={event => {
+            //Hotkey to select all text
+            if (event.key === 'a' && event.ctrlKey) {
+              Transforms.select(editor, {
+                anchor: Editor.start(editor, []),
+                focus: Editor.end(editor, []),
+              })
+            }
             for (const hotkey in HOTKEYS) {
                 if (isHotkey(hotkey, event)) {
                     event.preventDefault()
